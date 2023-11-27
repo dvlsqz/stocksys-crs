6283c1ef6d4c7d67911be767abc7abce6894f8c8
Daniel Velasquez <danielvel1977@gmail.com>
2023-11-26 19:45:45 -0600

    eliminacion de archivos sustituidos

--- codigo/resources/views/admin/bodega/bodega_principal/formulario.blade.php
+++
@@ -1,5 +0,0 @@
-<label for="name"> <strong><sup ><i class="fa-solid fa-triangle-exclamation"></i></sup> Insumo: </strong></label>
-<div class="input-group">
-    <span class="input-group-text" id="basic-addon1"><i class="fas fa-keyboard"></i></span>
-    {!! Form::select('id_insumo', $insumos, 0,['class'=>'form-select', 'id' => 'id_institucion', 'style' => 'width: 90%']) !!}
-</div>
