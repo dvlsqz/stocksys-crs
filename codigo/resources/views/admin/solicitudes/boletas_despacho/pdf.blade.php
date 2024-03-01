0c02fc592ceb11ecbe85993e6c7a9847db74a2fb
dev-igss <danielvel1977@gmail.com>
2024-03-01 15:45:17 -0600

    relacion de bodega egreso con ruta

--- codigo/resources/views/admin/solicitudes/boletas_despacho/pdf.blade.php
+++ codigo/resources/views/admin/solicitudes/boletas_despacho/pdf.blade.php
@@ -1,5 +1,6 @@
 <html lang="en">
 <head>
+    <title>Boleta de Despacho </title>
     <style>
         @page {
             size: 140mm 216mm landscape;
@@ -34,7 +35,7 @@
     @foreach($despachos as $d)
         
 
-        <div style="position: relative; top: 31.8mm; left: 48mm;  width: 250px; height: 10px; font-size: 12px;" >
+        <div style="position: relative; top: 31.8mm; left: 48mm;  width: 250px; height: 10px; font-size: 8px;" >
             {{ $d->escuela->nombre}}           
         </div>
 
@@ -42,11 +43,11 @@
             {{ $d->escuela->director }}  
         </div>
 
-        <div style="position: relative; top: 21.3mm; left: 153.2mm;  width: 250px; height: 10px; font-size: 12px;" >
+        <div style="position: relative; top: 5.3mm; left: 153.2mm;  width: 250px; height: 10px; font-size: 12px;" >
             {{ $d->escuela->codigo}}           
         </div>
 
-        <div style="position: relative; top: 21.2mm; left: 156mm;  width: 250px; height: 10px; font-size: 12px;">
+        <div style="position: relative; top: 13.2mm; left: 156mm;  width: 250px; height: 10px; font-size: 12px;">
             {{ $d->racion->nombre}}           
         </div>
 
@@ -55,26 +56,50 @@
         </div>
 
         <div style="position: relative; top: 38mm; left: 150mm;  width: 250px; height: 10px; font-size: 12px;"  >
-            {{ $d->escuela->ruta_asignada->ruta->ubicacion->nomenclatura.'0'.$d->escuela->ruta_asignada->ruta->correlativo }}           
+            {{ $d->ruta_asignada->ruta_solicitud->nombre}}           
+        </div>
+
+        <div style="position: relative; top: 34.2mm; left: 48.2mm;  width: 250px; height: 5px; font-size: 12px;" >
+            29/02/2024  
+        </div>
+
+        <div style="position: relative; top: 42.2mm; left: 45mm;  width: 100px; height: 10px; font-size: 12px; "  >
+            {{ $d->ruta_asignada->ruta_solicitud->tipo_vehiculo}}           
+        </div>
+
+        <div style="position: relative; top: 46.5mm; left: 43.3mm;  width: 175px; height: 10px; font-size: 12px;"  >
+            {{ $d->ruta_asignada->ruta_solicitud->nombre_piloto}}           
+        </div>
+
+        <div style="position: relative; top: 36.4mm; left: 96mm;  width: 100px; height: 10px; font-size: 12px;"  >
+            {{ $d->ruta_asignada->ruta_solicitud->placa_vehiculo}}           
+        </div>
+
+        <div style="position: relative; top: 40.6mm; left: 99mm;  width: 100px; height: 10px; font-size: 12px;"  >
+            {{ $d->ruta_asignada->ruta_solicitud->no_licencia}}           
         </div>
 
         @foreach($d->detalles as $det)
-            <div style="position: relative; top: 47mm; left: 17mm;  width: 250px; height: 10px; font-size: 12px;" >
+            <div style="position: relative; top: 52mm; left: 17mm;  width: 200px; height: 5px; font-size: 12px;" >
                 {{ $det->alimento_bodega_socio->nombre}} 
             </div>
                 
-            <div style="position: relative; top: 47mm; left: 90mm;  width: 250px; height: 10px; font-size: 12px;" >  
+            <div style="position: relative; top: 50mm; left: 90mm;  width: 100px; height: 10px; font-size: 12px;" >  
                 {{ $det->pl}}  
             </div>
 
-            <div style="position: relative; top: 47mm; left: 171mm;  width: 250px; height: 10px; font-size: 12px;" >
+            <div style="position: relative; top: 47mm; left: 140mm;  width: 100px; height: 10px; font-size: 12px;" >
+                {{ $det->no_unidades*100}}    
+            </div> 
+
+            <div style="position: relative; top: 45mm; left: 171mm;  width: 100px; height: 10px; font-size: 12px;" >
                 {{ $det->no_unidades}}    
             </div> 
             
         @endforeach        
 
-        <div style="position: relative; top: 101mm; left: 150mm;  width: 250px; height: 10px; font-size: 12px;"  >
-            {{ $d->escuela->ruta_asignada->ruta->ubicacion->nomenclatura.'0'.$d->escuela->ruta_asignada->ruta->correlativo }}           
+        <div style="position: relative; top: 70mm; left: 90mm;  width: 250px; height: 10px; font-size: 12px;"  >
+            Observaciones          
         </div>
         
         
